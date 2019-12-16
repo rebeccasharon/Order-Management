92c0b0203bb837fbef593785d260ebea6091da62
rebeccasharon <39788190+rebeccasharon@users.noreply.github.com>
2019-12-16 12:10:15 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,21 +1,8 @@
 <?php
 
-/*return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-]; */
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=Rebecca_Assignment;port=3306',
+    'dsn' => 'mysql:host=localhost;dbname=order_management;port=3306',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8'
